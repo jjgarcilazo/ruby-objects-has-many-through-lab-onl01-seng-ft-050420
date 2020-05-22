7b6ebfbcf973f749db0ac621d63f714a53d263eb
Jesse Garcilazo <jjgarcilazo@gmail.com>
2020-05-22 15:06:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -8,14 +8,14 @@ class Patient
     @@all << self
   end
 
-  def self.all
-    @@all
-  end
-
   def new_appointment(doctor, date)
     Appointment.new(self, date, doctor)
   end
 
+  def self.all
+    @@all
+  end
+  
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end
